a11da9f97f7ebc6a504dc74250a7925b69a4ea8c
Rafael Moreti Santana <rafaelmoretisantana@gmail.com>
2017-03-31 20:01:41 -0300

    initial commit

---
+++ src/main/java/com/rafael/PocMultiTenantApplication.java
@@ -0,0 +1,12 @@
+package com.rafael;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocMultiTenantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocMultiTenantApplication.class, args);
+	}
+}
